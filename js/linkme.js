d4c0925d467b6c7a02b763119b5593d3f3cbf5e4
Helder Calazans <heldercalazanss@gmail.com>
2020-01-22 22:04:34 -0300

    Modificaçao para que seja utilizado o timestamp como identificador da mensagem no banco

--- js/linkme.js
+++ js/linkme.js
@@ -1,11 +1,13 @@
 function enviarEmail() {
-    var emailValue = document.getElementById('emailTxt').value;
-    var mensagemValue = document.getElementById('areaTxt').value;
+    let emailValue = document.getElementById('emailTxt').value;
+    let mensagemValue = document.getElementById('areaTxt').value;
+    let timestamp = new Date().getTime();
+
 
     // Conectar com o firebase
     // Objeto db recebe funções de acesso ao banco de dados
-    var database = firebase.database();
-        database.ref('usuario').set({
+    let database = firebase.database();
+        database.ref(timestamp).set({
         email: emailValue,
         mensagem: mensagemValue
     });
