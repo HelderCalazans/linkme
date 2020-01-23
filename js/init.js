f7741cae3cd1b87cec48b8339baa7477961f5ca1
Helder Calazans <heldercalazanss@gmail.com>
2020-01-22 21:35:59 -0300

    Iniciando script para salvar mensagem do usuario no banco do firebase

---
+++ js/init.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCJfPE0uaQ61ztwvvtdrC0WMZWT2a2ioTU",
+    authDomain: "linkme-59356.firebaseapp.com",
+    databaseURL: "https://linkme-59356.firebaseio.com",
+    projectId: "linkme-59356",
+    storageBucket: "linkme-59356.appspot.com",
+    messagingSenderId: "951162143833",
+    appId: "1:951162143833:web:de26409b280d69e5dd5d48"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ js/linkme.js
@@ -0,0 +1,7 @@
+function enviarEmail() {
+    var email = document.getElementById('emailTxt').value;
+    var mensagem = document.getElementById('areaTxt').value;
+
+    console.log(email);
+    console.log(mensagem);  
+}
